c3e45532048cfc7092defc23a0c29318c91366a8
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-06-10 19:19:26 -0500

    Modifying the memory game (programming-oriented)

--- connect-4/script.js
+++ connect-4/script.js
@@ -6,129 +6,128 @@ p1_turn = true;
 nrows = 9;
 ncols = 7;
 state = matrix(nrows, ncols);
-lastAvailableRows = new Array(ncols).fill(nrows-1);
+lastAvailableRows = new Array(ncols).fill(nrows - 1);
 // console.log(lastAvailableRows)
 
-function drop_token(col){
-	if (lastAvailableRows[col] >= 0){
-		let row = lastAvailableRows[col]
-		if (p1_turn){
-			state[row][col] = 1;
-			document.querySelector(`tr:nth-child(${row + 1}) td:nth-child(${col+1})`).style.backgroundImage = `url(${p1Img})`;
-		}
-		else{
-			state[row][col] = 2;
-			document.querySelector(`tr:nth-child(${row + 1}) td:nth-child(${col+1})`).style.backgroundImage = `url(${p2Img})`;
-		}
-		if (check_winning_state([row, col])){
-			if (p1_turn)
-				displayGameOverMsg('PLAYER 1')
-			else
-				displayGameOverMsg('PLAYER 2')
-		}
-		p1_turn = !p1_turn;
-		lastAvailableRows[col] -= 1;
-	}
+function drop_token(col) {
+  if (lastAvailableRows[col] >= 0) {
+    let row = lastAvailableRows[col]
+    if (p1_turn) {
+      state[row][col] = 1;
+      document.querySelector(`tr:nth-child(${row + 1}) td:nth-child(${col+1})`).style.backgroundImage = `url(${p1Img})`;
+    } else {
+      state[row][col] = 2;
+      document.querySelector(`tr:nth-child(${row + 1}) td:nth-child(${col+1})`).style.backgroundImage = `url(${p2Img})`;
+    }
+    if (check_winning_state([row, col])) {
+      if (p1_turn)
+        displayGameOverMsg('PLAYER 1')
+      else
+        displayGameOverMsg('PLAYER 2')
+    }
+    p1_turn = !p1_turn;
+    lastAvailableRows[col] -= 1;
+  }
 }
 
-function displayGameOverMsg(winner){
-	document.querySelector('#winner').innerHTML = winner;
-	document.querySelector('#final-message').style.display = 'block';
+function displayGameOverMsg(winner) {
+  document.querySelector('#winner').innerHTML = winner;
+  document.querySelector('#final-message').style.display = 'block';
 }
 
 // uses the state and the last move
-function check_winning_state(last_move){
-	let [row, col] = last_move
-	
-	pid = 1;
-	if (!p1_turn){
-		pid = 2;
-	}
-	
-	// verifica la fila
-	let i = 1;
-	counter = 1;
-	while(state[row][col-i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	i=1;
-	while(state[row][col+i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	console.log(`row:${counter}`);
-	//verifica la columna
-	i=1;
-	counter = 1;
-	while(state[row+i] && state[row+i][col] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	console.log(`column:${counter}`);
-	// verifica la diagonal principal
-	i=1;
-	counter = 1;
-	while(state[row-i] && state[row-i][col-i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	i=1;
-	while(state[row+i] && state[row+i][col+i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	console.log(`princ:${counter}`);
-	// verifica la otra diagonal
-	i=1;
-	counter = 1;
-	while(state[row+i] && state[row+i][col-i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	i=1;
-	while(state[row-i] && state[row-i][col+i] == pid && i<4){
-		counter += 1;
-		i+=1;
-	}
-	if(counter >= 4){
-		console.log('win');
-		return true;
-	}
-	console.log(`second:${counter}`);
+function check_winning_state(last_move) {
+  let [row, col] = last_move
+
+  pid = 1;
+  if (!p1_turn) {
+    pid = 2;
+  }
+
+  // verifica la fila
+  let i = 1;
+  counter = 1;
+  while (state[row][col - i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  i = 1;
+  while (state[row][col + i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  console.log(`row:${counter}`);
+  //verifica la columna
+  i = 1;
+  counter = 1;
+  while (state[row + i] && state[row + i][col] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  console.log(`column:${counter}`);
+  // verifica la diagonal principal
+  i = 1;
+  counter = 1;
+  while (state[row - i] && state[row - i][col - i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  i = 1;
+  while (state[row + i] && state[row + i][col + i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  console.log(`princ:${counter}`);
+  // verifica la otra diagonal
+  i = 1;
+  counter = 1;
+  while (state[row + i] && state[row + i][col - i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  i = 1;
+  while (state[row - i] && state[row - i][col + i] == pid && i < 4) {
+    counter += 1;
+    i += 1;
+  }
+  if (counter >= 4) {
+    console.log('win');
+    return true;
+  }
+  console.log(`second:${counter}`);
 }
 
 
 document.querySelectorAll('tr').forEach(
-	item => [...item.cells].forEach(
-		(cell, col) => cell.addEventListener(
-			'click',
-			() => drop_token(col)
-			)
-		)
-	);
+  item => [...item.cells].forEach(
+    (cell, col) => cell.addEventListener(
+      'click',
+      () => drop_token(col)
+    )
+  )
+);
 // document.querySelectorAll('td:nth-child(1)').forEach(item => item.addEventListener('click', () => console.log('ga')));
--- memory/script.js
+++ memory/script.js
@@ -1,60 +1,70 @@
-defaultImg = 'wwo logo.png';
-foundImg = 'white.png';
-imgs = ['hacked.png', 'shiffman.jpg', 'talin.JPG', 'castorcito.jpg'];
-cardRevealedCount = 0;
-cardsRevealed = [undefined, undefined];
+const IMG_PATH = 'img/languages-frameworks/'
+const cards = document.querySelectorAll('.card');
+const defaultImg = 'the-matrix.png';
+const foundImg = 'white.png';
+const imgs = ['hacked.png', 'shiffman.jpg', 'talin.JPG', 'castorcito.jpg'];
+const pairs = [
+  ['java.png', 'spring.png'],
+  ['ruby.png', 'ruby-on-rails.png'],
+  ['sql.png', 'mysql.png'],
+  ['js.png', 'vue.png'],
+  ['python.png', 'django.png'],
+  ['php.png', 'laravel.png'],
+  // ['dart.png', 'flutter.png'],
+]
+const pointsToWin = pairs.length;
 
-cardsImgs = [0, 0, 1, 1, 2, 2, 3, 3];
-pointsToWin = 4;
-points = 0;
+let cardRevealedCount = 0;
+let cardsRevealed = [undefined, undefined];
+
+// cardsImgs = [0, 0, 1, 1, 2, 2, 3, 3];
+let cardsImgs = [...Array(12).keys()];
+
+let points = 0;
 shuffle(cardsImgs);
-cardsImgs = cardsImgs.map(item => `img/${imgs[item]}`);
+// cardsImgs = cardsImgs.map(item => `img/${imgs[item]}`);
+
+cardsImgs = cardsImgs.map(item => pairs[Math.floor(item / 2)][item % 2]);
+// cards.forEach((card, i) => card.style.backgroundImage = `url(${IMG_PATH}${cardsImgs[i]})`);
 // alert(cardsImgs);
-handlers = []
-// for(let i=0; i<8; i++){
-// 	handlers[i]=function(){clickHandler(`c${i}`)}
-// }
+handlers = [];
 
-// document.querySelectorAll('.card').forEach(item => {
-//   item.addEventListener('click', handlers[item.id[1]]);
-// });
-document.querySelectorAll('.card').forEach(item => {
-  item.addEventListener('click', handlers[item.id[1]] = function() { clickHandler(item.id) });
+cards.forEach((item, i) => {
+  item.addEventListener('click', handlers[i] = function() { clickHandler(i) });
 });
-
 
 function clickHandler(id) {
   if (cardRevealedCount == 2) {
     return;
   }
-  card = document.querySelector(`#${id}`);
+  card = cards[id];
   if (card === cardsRevealed[0]) {
     return;
   }
   console.log(card);
   // reveal
-  card.style.backgroundImage = `url(${cardsImgs[card.id[1]]})`;
+  card.style.backgroundImage = `url(${IMG_PATH}${cardsImgs[id]})`;
   cardsRevealed[cardRevealedCount] = card;
 
   cardRevealedCount += 1;
   console.log(cardsRevealed)
   if (cardRevealedCount == 2) {
     document.querySelector('#match-info').style.animation = 'none';
-    if (cardsRevealed[0].style.backgroundImage === cardsRevealed[1].style.backgroundImage) {
+    if (checkMatch(cardsRevealed[0], cardsRevealed[1])) {
       setTimeout(() => {
         document.querySelector('#match-info').innerHTML = 'You found a match! :D';
         document.querySelector('#match-info').style.animation = 'xd 2s linear 2 alternate';
 
         console.log('matches');
         cardsRevealed[0].style.backgroundImage = `url('img/${foundImg}')`;
-        cardsRevealed[0].removeEventListener('click', handlers[cardsRevealed[0].id[1]]);
+        cardsRevealed[0].removeEventListener('click', handlers[[...cards].indexOf(cardsRevealed[0])]);
         cardsRevealed[1].style.backgroundImage = `url('img/${foundImg}')`;
-        cardsRevealed[1].removeEventListener('click', handlers[cardsRevealed[1].id[1]]);
+        cardsRevealed[1].removeEventListener('click', handlers[[...cards].indexOf(cardsRevealed[1])]);
         cardRevealedCount = 0;
         cardsRevealed = [undefined, undefined]
         points += 1;
 
-        if (check_win()) {
+        if (checkWin()) {
           displayWinMsg();
         }
       }, 1000);
@@ -66,14 +76,23 @@ function clickHandler(id) {
         cardRevealedCount = 0;
         cardsRevealed = [undefined, undefined]
       }, 1000);
-
     }
-    // hide
-    // card.style.backgroundImage = `url(${defaultImg})`;
-    // if matches
-    // card.style.backgroundImage = `url(${foundImg})`;
-
   }
+}
+
+function checkMatch(cardA, cardB) {
+  // Disclaimer: It is assumed that cardA and cardB are different
+  // We extract the index of both cards
+  let indA = [...cards].indexOf(cardA);
+  let indB = [...cards].indexOf(cardB);
+  console.log(indA, indB);
+  // We extract the pairs corresponding to each card
+  let a = pairs.filter(pair => pair.includes(cardsImgs[indA]))
+  let b = pairs.filter(pair => pair.includes(cardsImgs[indB]))
+  // The next line means both pairs are different, so it's not a match
+  if (a[0] != b[0]) return false;
+  // Then, we found a match!
+  return true;
 }
 
 function shuffle(array) {
@@ -83,7 +102,7 @@ function shuffle(array) {
   }
 }
 
-function check_win() {
+function checkWin() {
   console.log(points);
   console.log(pointsToWin);
   return points === pointsToWin;
